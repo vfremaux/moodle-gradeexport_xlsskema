10a951d62468992aff5f52ddbddfc77ff23bad86
Valery Fremaux <valery.fremaux@gmail.com>
2023-09-13 14:45:48 +0200

    Automated sync from SVN

---
+++ lang/en/gradeexport_xlsskema.php
@@ -0,0 +1,37 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'gradeexport_xls', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   gradeexport_xls
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['eventgradeexported'] = 'XLS grade exported';
+$string['pluginname'] = 'Excel spreadsheet for Skema';
+$string['privacy:metadata'] = 'The Skema spreadsheet grade export plugin does not store any personal data.';
+
+$string['timeexported'] = 'Last downloaded from this course';
+$string['courseshort'] = 'Course shortname';
+$string['courseidnum'] = 'Course idnumber';
+$string['groupname'] = 'Group name';
+$string['groupidnum'] = 'Group idnumber';
+
+$string['xlsskema:publish'] = 'Publish XLS grade export';
+$string['xlsskema:view'] = 'Use Excel grade export';
